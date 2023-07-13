ae663465c1f5bdedca90fecc61cff719fe7be15a
asheesh1976 <ash_imt@yahoo.com>
2023-07-13 18:02:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-algo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-exmtc9)
---
+++ index.js
@@ -0,0 +1,19 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+/* 
+Given a string, create a new string made up of its last two letters, revered and seperated by a space.
+Example, given a word 'bat', return 't a'
+*/
+
+function reverseStr(str){
+
+    return str
+}
+
+
+appDiv.innerHTML = reverseStr("bat");
